fe0f7affcb0e48cf4976db611ec50d8fbb6abbfe
YoungKwanK <young.kwin@gmail.com>
2025-08-18 16:25:11 +0900

    2025-08-18

--- product/src/main/java/com/beyond/order/ordering/domain/OrderStatus.java
+++
@@ -1,6 +0,0 @@
-package com.beyond.order.ordering.domain;
-
-
-public enum OrderStatus {
-    ORDERED, CANCELED
-}
--- product/src/main/java/com/beyond/order/ordering/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.order.ordering.repository;
-
-import com.beyond.order.ordering.domain.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-}
